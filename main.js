67e3f026bf3a04546df0cbca5423591b66d36d88
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-02-07 11:13:14 -0500

    Initial Repo
    beginning exercises

---
+++ main.js
@@ -0,0 +1,11 @@
+                            /* LOOPS Exercise #1 */
+                    
+            // Use the following array:
+
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+            // Use a for loop to print each student's name to the console.
+
+for (let cOneS = 0; cOneS < cohortOneStudents.length; cOneS++) {
+    console.log(cohortOneStudents[cOneS]); 
+}
